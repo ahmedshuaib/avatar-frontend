be48b34632b985d9635b02c4d56e770f0a952390
user <user@users-MacBook-Pro.local>
2022-05-23 20:08:17 +0600

    added Avatar projectclear

---
+++ assets/js/test.js
@@ -0,0 +1,21 @@
+const burger=document.querySelector('.burger')
+const nav=document.querySelector('.nav-links')
+const navLinksFade = document.querySelectorAll('.nav-links li');
+const btns=document.querySelector('.btn-group')
+burger.addEventListener('click',function(){
+    burger.classList.toggle('toggle');
+    nav.classList.toggle('active');
+    btns.classList.toggle('active')
+    navLinksFade.forEach(function(link,index){
+        if(link.style.animation){
+            link.style.animation='';
+        }
+        else{
+            link.style.animation=` navLinkFade 0.5s ease forwards ${index / 8}s`;
+        }
+    })
+
+})
+
+
+
