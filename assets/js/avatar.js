52688483e6e1a771bc3d3ece906f34a7b143ce7d
user <user@users-MacBook-Pro.local>
2022-06-09 12:27:56 +0600

    added another files

--- assets/js/avatar.js
+++ assets/js/avatar.js
@@ -1,11 +1,9 @@
 const burger=document.querySelector('.burger')
 const nav=document.querySelector('.nav-links')
 const navLinksFade = document.querySelectorAll('.nav-links li');
-const btns=document.querySelector('.btn-group')
 burger.addEventListener('click',function(){
     burger.classList.toggle('toggle');
     nav.classList.toggle('active');
-    btns.classList.toggle('active')
     navLinksFade.forEach(function(link,index){
         if(link.style.animation){
             link.style.animation='';
