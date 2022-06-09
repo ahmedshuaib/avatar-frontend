5b6dbcef123c88e7ec0048650e7ce48914925cdf
user <user@users-MacBook-Pro.local>
2022-06-10 00:16:00 +0600

    added home page css

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -2,23 +2,6 @@ const dashBoard=document.querySelector('.dash');
 const list=document.querySelectorAll('.list')
 const bodyDash=document.querySelectorAll('.body-dash')
 
-
-// dashBoard.addEventListener('click',function(e){
-//     const id=e.target.dataset;
-//     if(id){
-//         list.forEach(function(list){
-//             list.classList.remove('active');
-//             e.target.classList.add('active');
-//         })
-//         bodyDash.forEach(function(dash){
-//             dash.classList.remove('active');
-//         })
-//         const element=document.getElementById(id);
-//         element.classList.add('active');
-//     }
-
-// })
-
 dashBoard.addEventListener('click',function(e){
     const id=e.target.dataset.id;
     if(id){
